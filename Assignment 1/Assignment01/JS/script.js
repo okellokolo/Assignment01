7e246e3f610cfb64667a3f24db05674aa7363ab2
okellokolo <okellokolo@gmail.com>
2026-09-21 10:28:27 -0700

    step 4 completed

--- Assignment 1/Assignment01/JS/script.js
+++ Assignment 1/Assignment01/JS/script.js
@@ -4,3 +4,32 @@ function theMonth() {}
 let currentMonth;
 let summerMonth;
 let myLibraryFunction;
+
+// STEP 2
+//Numeric literal
+let a = 23;
+//String literal
+let b = "Hello world";
+//Boolean literal
+let c = true;
+let d = false;
+//A null
+let e = 78;
+x = null;
+
+//STEP 3
+let f = 15;
+let g = 3;
+let h = f * g + 3;
+
+//STEP 4
+let firstName;
+let lastName;
+let address;
+let city;
+let zipCode;
+let yourAge;
+let referalSource;
+let contactYou;
+
+//STEP 5
