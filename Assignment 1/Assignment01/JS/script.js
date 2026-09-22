5d7d38de3eb567476b4e8cd274eecb2042d8854a
okellokolo <okellokolo@gmail.com>
2026-09-21 17:30:37 -0700

    Completed step 9

--- Assignment 1/Assignment01/JS/script.js
+++ Assignment 1/Assignment01/JS/script.js
@@ -38,3 +38,44 @@ let scontactYou;
 let someMonthfirstName = "Charles";
 var sLastName = "Okello";
 const nZipCode = "92111";
+
+//STEP 6
+let theResult = 17 + " here is the string";
+console.log(theResult);
+
+//STEP 7
+// The first variable, adding a Boolean and a String
+let theResults = true + " is the answer";
+
+// Second variable
+let secondResult = "The coerced result is: ";
+
+// Display the coerced result
+console.log(secondResult + theResults);
+
+//STEP 8
+let someString =
+  'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."';
+console.log(someString);
+
+//STEP 9
+//A variable that produce null
+let x = 23;
+x = null;
+alert(x); //null
+//A variable that produce undefined
+y = 11;
+alert(y); //undefined
+
+//STEP 10
+console.log(typeof "Hello World");
+// Output: "string"
+
+console.log(typeof "21");
+// Output: "number"
+
+console.log(typeof false);
+// Output: "boolean"
+
+console.log(typeof undeclared);
+// Output: "undefined"
